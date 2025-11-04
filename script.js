12a63ad0846c3ca138113246fd7c6abca4f4ef41
4drea4 <bakerand@outlook.com>
2025-11-04 13:51:17 -0600

    added list item and price to the list

--- script.js
+++ script.js
@@ -3,12 +3,27 @@ const productPriceInput = document.getElementById('product-price');
 const addProductButton = document.getElementById('add-product');
 const cart = document.getElementById('cart');
 const totalPriceSpan = document.getElementById('total-price');
-const shopList = document.getElementById('list');
+const shopList = document.querySelector('.list');
 const counter =document.getElementById('item-count');
 
  
 let totalPrice = 0;
  
+// Function to add the Price and the Product Item Name to the Ul
+ 
+addProductButton.addEventListener('click', () => { //created event listener for input button
+ const productName = productNameInput.value.trim(); //declaring variable to take the name
+ const itemPrice = productPriceInput.value; //declaring variable to collect the price.
+
+//creating the new list item 
+const li = document.createElement('li'); //creating the list item
+li.textContent = productName + '$' + itemPrice; //concatenation to store the item and price
+
+// showing list item and price to the screen. 
+shopList.appendChild(li);
+});
+
+
 // Function to update the total price
 function updateTotalPrice(amount) {
   totalPrice += amount;
